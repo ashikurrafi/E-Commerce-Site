d6fb8c39e8f0fb4681456a1bbbfcaf9cada9b711
ashikurrafi <ashikurrafi@gmail.com>
2023-12-21 10:05:02 +0600

    Test API created

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: "Welcome to server",
+    });
+});
+
+app.get("/test", (req, res) => {
+    res.send("Test API is working [GET]");
+});
 
 port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running at http:localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
